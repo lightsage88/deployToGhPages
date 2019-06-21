e4c519bf97e8fe1a58385553e026dfe1287a1603
Adrian Rosales <adrian.e.rosales@gmail.com>
2019-06-21 12:17:29 -0700

    Updates

---
+++ precache-manifest.89a1cf1a99bab9fc615b221e99a9cf3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44c3e97e123f12fa9dc0",
+    "url": "/deployToGhPages/static/js/runtime~main.44c3e97e.js"
+  },
+  {
+    "revision": "925895e43c03588b17d7",
+    "url": "/deployToGhPages/static/js/main.925895e4.chunk.js"
+  },
+  {
+    "revision": "bcc2869513a80bfb7633",
+    "url": "/deployToGhPages/static/js/1.bcc28695.chunk.js"
+  },
+  {
+    "revision": "925895e43c03588b17d7",
+    "url": "/deployToGhPages/static/css/main.a7d07613.chunk.css"
+  },
+  {
+    "revision": "98ce260ad409776462ab6bf532c570eb",
+    "url": "/deployToGhPages/index.html"
+  }
+];
